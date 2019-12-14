ce02a2a308c09c821c98b6f938f80164e67d6b05
Bui <bui@bui.pm>
2019-12-14 10:48:50 +0000

    https

--- remocolab.py
+++ remocolab.py
@@ -87,7 +87,7 @@ def _setupSSHDImpl(ngrok_token, ngrok_region):
     raise RuntimeError("Failed to run ngrok. Return code:" + str(ngrok_proc.returncode) + "\nSee runtime log for more info.")
 
   with urllib.request.urlopen("http://localhost:4040/api/tunnels") as response:
-    url = json.load(response)['tunnels'][1]['public_url']
+    url = json.load(response)['tunnels'][1]['public_url'].replace('http://', 'https://')
 
   _log('Setting up noVNC...')
   subprocess.run(['git', 'clone', 'https://github.com/novnc/noVNC.git'])
