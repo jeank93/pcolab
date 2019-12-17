882c15c56ddd5781cf4370684d9026a66934fef5
LoveEevee <LoveEevee@users.noreply.github.com>
2019-12-17 18:26:40 +0300

    Kill processes on exit and do not redownload
    The HTTP proxy option in 3proxy seems to be broken

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# remocolab
+# Remocolab Proxy
 [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/LoveEevee/remocolab/blob/master/remocolab.ipynb)
 
 A fork of [remocolab](https://github.com/bui/remocolab) that ditches noVNC support and sets up a proxy with [3proxy](https://github.com/z3APA3A/3proxy) instead.
--- remocolab.py
+++ remocolab.py
@@ -1,7 +1,6 @@
 import pathlib, stat, shutil, urllib.request, subprocess, time
 import json, sys
-import os, tarfile
-
+import os, tarfile, psutil
 
 def _log(message):
 	print("[%s] %s" % (time.strftime("%H:%M:%S", time.localtime()), message))
@@ -15,29 +14,38 @@ def _download(url, path):
 		print("Failed to download ", url)
 		raise
 
-def _setupSSHD(region, token):
-	_log("Downloading and installing ngrok...")
-	_download("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip", "ngrok.zip")
-	shutil.unpack_archive("ngrok.zip")
-	pathlib.Path("ngrok").chmod(stat.S_IXUSR)
+def _killproc(name):
+	for proc in psutil.process_iter():
+		if proc.name() == name:
+			proc.kill()
+
+def _setupSSHD(token, region):
+	if not os.path.isfile("./ngrok"):
+		_log("Downloading and installing ngrok...")
+		_download("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip", "ngrok.zip")
+		shutil.unpack_archive("ngrok.zip")
+		os.remove("ngrok.zip")
+		pathlib.Path("ngrok").chmod(stat.S_IXUSR)
+	else:
+		_killproc("ngrok")
 	
-	if not pathlib.Path("/root/.ngrok2/ngrok.yml").exists():
-		subprocess.run(["./ngrok", "authtoken", token])
+	ngrok_config = "/root/.ngrok2/ngrok.yml"
+	subprocess.run(["./ngrok", "authtoken", "-config", ngrok_config, token], check=True)
 	
 	_log("Creating ngrok tunnel...")
-	ngrok_proc = subprocess.Popen(["./ngrok", "tcp", "-region", region, "1080"])
+	ngrok_proc = subprocess.Popen(["./ngrok", "tcp", "-config", ngrok_config, "-region", region, "1080"])
 	time.sleep(2)
 	if ngrok_proc.poll() != None:
-		raise RuntimeError("Failed to run ngrok. Return code:" + str(ngrok_proc.returncode) + "\nSee runtime log for more info.")
+		raise RuntimeError("Failed to run ngrok. Return code: {0}\nSee runtime log for more info.".format(ngrok_proc.returncode))
 	
 	with urllib.request.urlopen("http://localhost:4040/api/tunnels") as response:
-		url = json.load(response)["tunnels"][0]["public_url"]
+		url = json.load(response)["tunnels"][-1]["public_url"]
 		if url.startswith("tcp://"):
 			url = url[len("tcp://"):]
 	
-	return url
+	return url, ngrok_proc
 
-def _setupProxy(url):
+def _setupProxy(url, protocol):
 	proxy3_ver = "0.8.13"
 	
 	proxy3_url = "https://github.com/z3APA3A/3proxy/archive/{0}.tar.gz".format(proxy3_ver)
@@ -47,40 +55,45 @@ def _setupProxy(url):
 	proxy3_cfgdir = "/usr/local/etc/3proxy"
 	proxy3_cfgfile = os.path.join(proxy3_cfgdir, "3proxy.cfg")
 	
-	_log("Downloading and installing 3proxy...")
-	_download(proxy3_url, "3proxy.tar.gz")
-	tar = tarfile.open("3proxy.tar.gz", "r:gz")
-	tar.extractall()
-	tar.close()
-	subprocess.run(["make", "-C", proxy3_dir, "-f", proxy3_makefile])
-	subprocess.run(["make", "-C", proxy3_dir, "-f", proxy3_makefile, "install"])
+	if not shutil.which("3proxy"):
+		_log("Downloading and installing 3proxy...")
+		_download(proxy3_url, "3proxy.tar.gz")
+		with tarfile.open("3proxy.tar.gz", "r:gz") as tar:
+			tar.extractall()
+		os.remove("3proxy.tar.gz")
+		
+		subprocess.run(["make", "-C", proxy3_dir, "-f", proxy3_makefile], check=True)
+		subprocess.run(["make", "-C", proxy3_dir, "-f", proxy3_makefile, "install"], check=True)
+		shutil.rmtree(proxy3_dir)
+	else:
+		_killproc("3proxy")
+	
 	pathlib.Path(proxy3_cfgdir).mkdir(parents=True, exist_ok=True)
 	with open(proxy3_cfgfile, "w+") as f:
 		proxy_config = [
-			"nserver 8.8.8.8",
-			"daemon",
-			"auth none",
-			"external 0.0.0.0",
-			"internal 0.0.0.0",
-			"flush",
-			"socks -aunp1080"
+			"proxy -aunp1080" if protocol == "HTTP" else "socks -aunp1080"
 		]
 		f.write("\n".join(proxy_config))
 		f.close()
 	
 	_log("Running the proxy server...")
-	subprocess.run(["3proxy", proxy3_cfgfile])
+	proxy3_proc = subprocess.Popen(["3proxy", proxy3_cfgfile])
+	time.sleep(1)
+	if proxy3_proc.poll() != None:
+		raise RuntimeError("Failed to run 3proxy. Return code: {0}\nSee runtime log for more info.".format(proxy3_proc.returncode))
 	
 	_log("Ready!")
 	index = url.find(":")
-	print("Protocol: SOCKS5")
+	print("Protocol: {0}".format(protocol))
 	if index != -1:
 		print("Server: {0}".format(url[:index]))
 		print("Port: {0}".format(url[index+1:]))
 	else:
 		print("Server: {0}".format(url))
+	
+	return proxy3_proc
 
-def setupProxy(region="", token=None):
+def setupProxy(token="", region="eu", protocol="SOCKS5"):
 	if not token:
 		sys.exit("ERROR: Invalid ngrok authtoken. Ensure you have copied the token from https://dashboard.ngrok.com/auth and try again.")
 	
@@ -89,11 +102,17 @@ def setupProxy(region="", token=None):
 		region = region[:index]
 	
 	_log("Starting...")
-	url = _setupSSHD(region, token)
-	_setupProxy(url)
+	url, ngrok_proc = _setupSSHD(token, region)
+	proxy3_proc = _setupProxy(url, protocol)
 	
 	try:
 		while True:
 			time.sleep(1)
 	except KeyboardInterrupt:
 		_log("Exiting...")
+		if ngrok_proc.poll() != None:
+			ngrok_proc.kill()
+		_killproc("ngrok")
+		if proxy3_proc.poll() != None:
+			proxy3_proc.kill()
+		_killproc("3proxy")
