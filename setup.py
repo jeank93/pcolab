bd9d59fac323458d36be3897b2b6bca255961cb0
jeank93 <jcarlos1993@hotmail.com>
2022-09-10 18:14:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "pcolab.py",
     version = "0.1",
-    py_modules = ['remocolab'],
+    py_modules = ['pcolab'],
     url = "https://github.com/demotomohiro/remocolab",
     author = "demotomohiro",
 )
